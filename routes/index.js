b70ccacb68604e7b20f5480ec5d58a80682e2af3
sanjanabaddukonda <114835671+sanjanabaddukonda@users.noreply.github.com>
2022-11-25 17:28:18 -0500

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home'
+  });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', {
+    title: "Home"
+  });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+    title: 'About'
+  });
+});
+
+/* GET products page. */
+router.get('/stats', function(req, res, next) {
+  res.render('stats', { 
+    title: 'Products'
+  });
+});
+
+/* GET services page. */
+router.get('/apply', function(req, res, next) {
+  res.render('apply', { 
+    title: 'apply'
+  });
+});
+
+
+
+module.exports = router;
